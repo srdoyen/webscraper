7b84ea8ac9c532e87c38f1bea9cbe883b9709656
Scott Doyen <srdoyen@gmail.com>
2021-08-15 13:54:08 -0700

    first commit

---
+++ webscraper.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+url = 'http://ethans_fake_twitter_site.surge.sh/'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+
+tweetArr = []
+for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
+    tweetObject = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text,
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
+        "tweet": tweet.find('p', attrs={"class": "content"}).text,
+        "likes": tweet.find('p', attrs={"class": "likes"}).text,
+        "shares": tweet.find('p', attrs={"class": "shares"}).text
+    }
+    tweetArr.append(tweetObject)
+with open('twitterData.json', 'w', encoding='utf-8') as outfile:
+    json.dump(tweetArr, outfile)
