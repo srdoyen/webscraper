f972d5639708ebe2398f2f5f00a4dcc13cdf82ba
Scott Doyen <srdoyen@gmail.com>
2021-08-15 15:21:59 -0700

    nurse job accumulator

--- webscraper.py
+++ webscraper.py
@@ -2,19 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = 'http://ethans_fake_twitter_site.surge.sh/'
+jobArr = []
+url = 'https://www.simplyhired.com/search?q=rn+new+grad+program&l=San+Francisco+Bay+Area%2C+CA'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-tweetArr = []
-for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
-    tweetObject = {
-        "author": tweet.find('h2', attrs={"class": "author"}).text,
-        "date": tweet.find('h5', attrs={"class": "dateTime"}).text,
-        "tweet": tweet.find('p', attrs={"class": "content"}).text,
-        "likes": tweet.find('p', attrs={"class": "likes"}).text,
-        "shares": tweet.find('p', attrs={"class": "shares"}).text
+for job in content.findAll('div', attrs={"class": "SerpJob-jobCard card"}):
+    jobObject = {
+        "jobName" : job.find('a', attrs={"class": "SerpJob-link card-link"}).text,
+        "company" : job.find('span', attrs={"class": "JobPosting-labelWithIcon jobposting-company"}).text,
+        "jobDescription": job.find('p', attrs={"class": "jobposting-snippet"}).text
     }
-    tweetArr.append(tweetObject)
-with open('twitterData.json', 'w', encoding='utf-8') as outfile:
-    json.dump(tweetArr, outfile)
+    if("dialysis" not in jobObject["jobDescription"] and "Skilled Nursing" not in jobObject["jobDescription"] and "sub-acute" not in jobObject["jobDescription"]):
+        jobArr.append(jobObject)
+
+url = 'https://www.indeed.com/jobs?q=New%20Grad%20RN%20Opportunity%20-LVN&l=San%20Francisco%20Bay%20Area%2C%20CA'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+for job in content.findAll('div', attrs={"class": "job_seen_beacon"}):
+    jobObject = {
+        "jobName" : job.find('h2', attrs={"class": "jobTitle"}).text,
+        "company" : job.find('span', attrs={"class": "companyName"}).text,
+        "jobDescription": job.find('li').text
+    }
+    if("dialysis" not in jobObject["jobDescription"] and "Skilled Nursing" not in jobObject["jobDescription"] and "sub-acute" not in jobObject["jobDescription"]):
+        jobArr.append(jobObject)
+
+
+with open('jobData.json', 'w', encoding='utf-8') as outfile:
+    json.dump(jobArr, outfile)
